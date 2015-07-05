945b9f72f76546f42a8ca92fa86eefb553dedb21
jsvhqr <jsvhqr@yahoo.com>
2015-07-05 11:13:26 +0200

    changed some..

--- index.php
+++ index.php
@@ -6,26 +6,41 @@
 
 <title>JSN HOME</title>
 </head>
-<body class="body">
-
-
-	<div id="headerContainerDiv" class="headerClass">
-		<img alt="headerImage" src="images/headerimg.jpg">
+<body>
+	<div id="header">
+		<div id="mainPageHeading">
+			<h1>JSN under construction</h1>
+		</div>
+	
 		<?php
 		if (isset ( $_SESSION ['loggedin'] ) && $_SESSION ['loggedin'] == TRUE && $_SESSION ['ip'] == $_SERVER ['REMOTE_ADDR']) {
 			?>
-		<div id="loggedInDiv" class="loggedInClass">
-			<h1>Welcome $_SESSION['username']</h1>
+		<div id="loggedInNav" class="Nav">
+			<ul class="Nav">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="user.php">$_SESSION['username']</a></li>
+				<li><form action="logout.php" name="logoutForm" method="post">
+						<label id="logoutLabel"><input type="submit" value="Log out"></label>
+					</form></li>
+			</ul>
 		</div>
 		<?php }else{?>
-		<div id="loggedOutDiv" class="loggedOutClass">
-			<h1>login</h1 >
+		<div id="loggedOutNav " class="Nav">
+			<ul class="Nav">
+				<li><a href="index.php">Home</a></li>
+				<li><form action="login.php" name="loginForm" method="post">
+						<label id="loginLabel"><input type="submit" value="Log in"></label>
+					</form></li>
+				<li><form action="register.php" name="registerForm" method="post">
+						<label id="registerLabel"><input type="submit" value="Register"></label>
+					</form></li>
+			</ul>
 		</div>
 		<?php }?>
 	</div>
 
 
-	<div id="page" class="page-section"></div>
+	<div id="content"></div>
 
 
 </body>
